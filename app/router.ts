291f3b69b0190de476cff80d3cf1b6ec63112ebb
jason <jason.cc@galaxyoversea.com>
2021-02-10 11:25:02 +0800

    优化接口路径

--- app/router.ts
+++ app/router.ts
@@ -4,7 +4,7 @@ export default (app: Application) => {
   const { controller, router } = app;
   router.get('/default/getArticleList', controller.default.home.getArticleList)
   router.get('/default/getArticleById/:id', controller.default.home.getArticleById)
-  router.get('/default/getTypeInof', controller.default.home.getTypeInof)
+  router.get('/default/getTypeInfo', controller.default.home.getTypeInof)
   router.get('/default/getListById',controller.default.home.getListById)
   router.get('/', controller.home.index);
 };
